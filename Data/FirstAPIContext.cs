73533845a327b52319600f19c7d6589b51d2bcc1
Yuki <celsonegreli@gmail.com>
2025-06-26 02:11:40 -0300

    Add project files.

---
+++ Data/FirstAPIContext.cs
@@ -0,0 +1,41 @@
+﻿using FirstAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Data
+{
+    public class FirstAPIContext : DbContext
+    {
+        public FirstAPIContext(DbContextOptions<FirstAPIContext> options):base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book
+                {
+                    Id = 1,
+                    Title = "Ordem Paranormal: Iniciação",
+                    Author = "Fabio Yabu",
+                    YearPublished = 2022
+                },
+                new Book
+                {
+                    Id = 2,
+                    Title = "Ordem Paranormal: O Segredo na Floresta - Parte 1",
+                    Author = "Fabio Yabu",
+                    YearPublished = 2023,
+                },
+                new Book
+                {
+                    Id = 3,
+                    Title = "Ordem Paranormal: O Segredo na Floresta - Parte 2",
+                    Author = "Fabio Yabu",
+                    YearPublished = 2024,
+                }
+            );
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Migrations/20250626044915_book data added.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FirstAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class bookdataadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "Title", "YearPublished" },
+                values: new object[,]
+                {
+                    { 1, "Fabio Yabu", "Ordem Paranormal: Iniciação", 2022 },
+                    { 2, "Fabio Yabu", "Ordem Paranormal: O Segredo na Floresta - Parte 1", 2023 },
+                    { 3, "Fabio Yabu", "Ordem Paranormal: O Segredo na Floresta - Parte 2", 2024 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
